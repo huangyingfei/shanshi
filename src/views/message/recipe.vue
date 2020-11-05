9c2916ca0f90e9a49cb9322e4fc007464f52aee7
liqy <liqy273>
2020-11-05 23:29:06 +0800

    hyf

--- src/views/message/recipe.vue
+++ src/views/message/recipe.vue
@@ -120,13 +120,13 @@
           label-width="105px"
           class="demo-ruleForm"
         >
-          <el-form-item label="部门名称" style="width:355px">
-            <el-input style="width:250px" v-model="storage.name"></el-input>
+          <el-form-item label="部门名称" style="width: 355px">
+            <el-input style="width: 250px" v-model="storage.name"></el-input>
           </el-form-item>
-          <el-form-item label="部门排序" style="width:355px">
+          <el-form-item label="部门排序" style="width: 355px">
             <el-input
               type="number"
-              style="width:250px"
+              style="width: 250px"
               v-model="storage.sorting"
             ></el-input>
           </el-form-item>
@@ -152,11 +152,11 @@
           label-width="105px"
           class="demo-ruleForm"
         >
-          <el-form-item label="子部门名称" style="width:355px">
-            <el-input style="width:250px" v-model="acetone.name"></el-input>
+          <el-form-item label="子部门名称" style="width: 355px">
+            <el-input style="width: 250px" v-model="acetone.name"></el-input>
           </el-form-item>
-          <el-form-item label="子部门排序" style="width:355px">
-            <el-input style="width:250px" v-model="acetone.sorting"></el-input>
+          <el-form-item label="子部门排序" style="width: 355px">
+            <el-input style="width: 250px" v-model="acetone.sorting"></el-input>
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
@@ -180,19 +180,19 @@
           label-width="105px"
           class="demo-ruleForm"
         >
-          <el-form-item label="姓名" style="width:355px">
-            <el-input style="width:250px" v-model="ruleForm.name"></el-input>
+          <el-form-item label="姓名" style="width: 355px">
+            <el-input style="width: 250px" v-model="ruleForm.name"></el-input>
           </el-form-item>
-          <el-form-item label="性别" style="width:355px">
+          <el-form-item label="性别" style="width: 355px">
             <el-radio
-              style="margin-left:30px"
+              style="margin-left: 30px"
               v-model="ruleForm.radio"
               label="1"
               >男</el-radio
             >
             <el-radio v-model="ruleForm.radio" label="2">女</el-radio>
           </el-form-item>
-          <el-form-item style="width:355px" label="图片">
+          <el-form-item style="width: 355px" label="图片">
             <el-upload
               action="https://jsonplaceholder.typicode.com/posts/"
               list-type="picture-card"
@@ -205,30 +205,30 @@
               <img width="100%" :src="dialogImageUrl" alt />
             </el-dialog>
           </el-form-item>
-          <el-form-item style="width:355px" label="婚姻状况">
+          <el-form-item style="width: 355px" label="婚姻状况">
             <el-radio
-              style="margin-left:30px"
+              style="margin-left: 30px"
               v-model="ruleForm.marriages"
               label="1"
               >已婚</el-radio
             >
             <el-radio v-model="ruleForm.marriages" label="2">未婚</el-radio>
           </el-form-item>
-          <el-form-item label="出生日期" style="width:355px">
+          <el-form-item label="出生日期" style="width: 355px">
             <el-date-picker
-              style="width:250px"
+              style="width: 250px"
               v-model="ruleForm.value1"
               type="date"
               placeholder="选择日期"
             >
             </el-date-picker>
           </el-form-item>
-          <el-form-item label="手机号码" style="width:355px">
-            <el-input style="width:250px" v-model="ruleForm.phones"></el-input>
+          <el-form-item label="手机号码" style="width: 355px">
+            <el-input style="width: 250px" v-model="ruleForm.phones"></el-input>
           </el-form-item>
-          <el-form-item label="民族" style="width:355px">
+          <el-form-item label="民族" style="width: 355px">
             <el-select
-              style="width:250px"
+              style="width: 250px"
               v-model="ruleForm.national"
               placeholder="请选择"
             >
@@ -241,9 +241,9 @@
               </el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="职务" style="width:355px">
+          <el-form-item label="职务" style="width: 355px">
             <el-select
-              style="width:250px"
+              style="width: 250px"
               v-model="ruleForm.position"
               placeholder="请选择"
             >
@@ -256,9 +256,9 @@
               </el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="所在年级/班级" style="width:355px">
+          <el-form-item label="所在年级/班级" style="width: 355px">
             <el-select
-              style="width:250px"
+              style="width: 250px"
               v-model="ruleForm.getClass"
               placeholder="请选择"
             >
@@ -271,17 +271,17 @@
               </el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="工号" style="width:355px">
-            <el-input style="width:250px" v-model="ruleForm.thejob"></el-input>
+          <el-form-item label="工号" style="width: 355px">
+            <el-input style="width: 250px" v-model="ruleForm.thejob"></el-input>
           </el-form-item>
-          <el-form-item style="width:355px">
-            <span style="font-size: 12px;"
+          <el-form-item style="width: 355px">
+            <span style="font-size: 12px"
               >若为年级组长、保教主任、老师、保育员时需选择 所在年级/班级</span
             >
           </el-form-item>
-          <el-form-item label="入职日期" style="width:355px">
+          <el-form-item label="入职日期" style="width: 355px">
             <el-date-picker
-              style="width:250px"
+              style="width: 250px"
               v-model="ruleForm.inductions"
               format="yyyy 年 MM 月 dd 日"
               value-format="yyyy-MM-dd"
@@ -290,9 +290,11 @@
             >
             </el-date-picker>
           </el-form-item>
-          <el-form-item label="参加工作日期" style="width:355px">
+          <el-form-item label="参加工作日期" style="width: 355px">
+            <!--  -->
             <el-date-picker
-              style="width:250px"
+              style="width: 250px"
+              @change="stop()"
               v-model="ruleForm.workin"
               format="yyyy 年 MM 月 dd 日"
               value-format="yyyy-MM-dd"
@@ -301,15 +303,18 @@
             >
             </el-date-picker>
           </el-form-item>
-          <el-form-item label="工龄" style="width:355px">
-            <el-input style="width:250px" v-model="ruleForm.process"></el-input>
+          <el-form-item label="工龄" style="width: 355px">
+            <el-input
+              style="width: 250px"
+              v-model="ruleForm.process"
+            ></el-input>
           </el-form-item>
-          <el-form-item label="邮箱" style="width:355px">
-            <el-input style="width:250px" v-model="ruleForm.emails"></el-input>
+          <el-form-item label="邮箱" style="width: 355px">
+            <el-input style="width: 250px" v-model="ruleForm.emails"></el-input>
           </el-form-item>
-          <el-form-item label="当前状态" style="width:355px">
+          <el-form-item label="当前状态" style="width: 355px">
             <el-select
-              style="width:250px"
+              style="width: 250px"
               v-model="ruleForm.ddeparture"
               placeholder="请选择"
             >
@@ -322,9 +327,9 @@
               </el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="状态更新日期" style="width:355px">
+          <el-form-item label="状态更新日期" style="width: 355px">
             <el-date-picker
-              style="width:250px"
+              style="width: 250px"
               v-model="ruleForm.nextstate"
               type="date"
               placeholder="选择日期"
@@ -476,11 +481,11 @@ export default {
       obtained: false, //子部门弹框
       acetone: {
         name: "", //子部门名称
-        sorting: "" //部门排序
+        sorting: "", //部门排序
       },
       storage: {
         name: "", //部门名称
-        sorting: "" //部门排序
+        sorting: "", //部门排序
       },
       ruleForm: {
         name: "", //姓名
@@ -499,150 +504,158 @@ export default {
         ddeparture: "", //当前状态
         nextstate: "", //状态更新日期
         desc: "",
-        resource: ""
+        resource: "",
       },
       tableData: [
         {
           date: "2016-05-02",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
+          address: "上海市普陀区金沙江路 1518 弄",
         },
         {
           date: "2016-05-04",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        }
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
       ],
       emailslist: [
         //当前状态
         {
           value: "1",
-          label: "在职"
+          label: "在职",
         },
         {
           value: "2",
-          label: "离职"
+          label: "离职",
         },
         {
           value: "3",
-          label: "停职"
+          label: "停职",
         },
         {
           value: "4",
-          label: "退休"
-        }
+          label: "退休",
+        },
       ],
       loadClass: [
         //
         {
           value: "1",
-          label: "中班"
+          label: "中班",
         },
         {
           value: "2",
-          label: "小班"
-        }
+          label: "小班",
+        },
       ],
       vposition: [
         //职务
         {
           value: "1",
-          label: "园长"
+          label: "园长",
         },
         {
           value: "2",
-          label: "老师"
-        }
+          label: "老师",
+        },
       ],
       college: [
         {
           value: "1",
-          label: "汉族"
+          label: "汉族",
         },
         {
           value: "2",
-          label: "回族"
-        }
+          label: "回族",
+        },
       ],
       prompt: [
         {
           value: "选项1",
-          label: "黄金糕"
+          label: "黄金糕",
         },
         {
           value: "选项2",
-          label: "双皮奶"
+          label: "双皮奶",
         },
         {
           value: "选项3",
-          label: "蚵仔煎"
-        }
+          label: "蚵仔煎",
+        },
       ],
       callback: "", //职务搜索
       mState: [
         {
           value: "1",
-          label: "全部"
+          label: "全部",
         },
         {
           value: "2",
-          label: "在职"
+          label: "在职",
         },
         {
           value: "3",
-          label: "离职"
+          label: "离职",
         },
         {
           value: "4",
-          label: "停职"
+          label: "停职",
         },
         {
           value: "5",
-          label: "退休"
-        }
+          label: "退休",
+        },
       ],
       driver: "", //当前状态
       number: [
         {
           value: "1",
-          label: "升序"
+          label: "升序",
         },
         {
           value: "2",
-          label: "降序"
-        }
+          label: "降序",
+        },
       ],
-      working: ""
+      working: "",
     };
   },
   beforeMount() {
     this.getStorage(); //获取部门树形结构
   },
   methods: {
-    //计算
+    //计算工龄
     stop() {
-      console.log(this.ruleForm.workin);
-      let add = this.ruleForm.workin.replace(/-/g, "");
-      console.log(add);
-      let topyear = add.substring(0, 4);
-      let topmonth = add.substring(4, 6);
-      let topday = add.substring(6, 9);
+      //  let add = this.ruleForm.workin.replace(/-/g, "");
+      //   console.log(add);
+      //   let topyear = add.substring(0, 4);
+      //   let topmonth = add.substring(4, 6);
+      //   let topday = add.substring(6, 9);
 
-      var myDate = new Date();
+      let add = this.ruleForm.workin;
+      // console.log(this.ruleForm.workin);
+
+      let afterTime = new Date(add).getTime();
+      var myDate = new Date().getTime();
+      let year = ((myDate-afterTime)/(24*60*60*1000*365)).toFixed(0);
+      console.log(year)
+      this.ruleForm.process = year;
+      // console.log(myDate);
       // let vtop = d.getFullYear();
-      console.log(myDate.getMonth());
-      // let myyear=myDate
-      let going = myDate.getFullYear() - topyear - 1;
+      // console.log(myDate.getMonth());
+
+      // let going = myDate.getFullYear() - topyear - 1;
+
+      // if (
+      //   topmonth < myDate.getMonth() ||
+      //   (topmonth == myDate.getMonth() && topday <= myDate.getDate())
+      // ) {
+      //   going++;
+      // }
       // console.log(going);
-      if (
-        topmonth < myDate.getMonth() ||
-        (topmonth == myDate.getMonth() && topday <= myDate.getDate())
-      ) {
-        going++;
-      }
-      console.log(going);
-      this.ruleForm.process = going;
+      // this.ruleForm.process = going;
     },
+
     //添加员工弹框
     addition() {
       this.dateTime = true;
@@ -663,9 +676,9 @@ export default {
         .post(`api/blade-food/teacherdept/submit`, {
           parentId: this.adds, //上级ID
           deptName: this.acetone.name, //部门名称
-          level: this.acetone.sorting //部门排序
+          level: this.acetone.sorting, //部门排序
         })
-        .then(res => {
+        .then((res) => {
           this.getStorage();
           this.obtained = false;
           this.acetone.name = "";
@@ -673,7 +686,7 @@ export default {
           console.log(res);
           this.$message({
             message: "添加成功",
-            type: "success"
+            type: "success",
           });
         })
         .catch(() => {
@@ -685,9 +698,9 @@ export default {
       this.$axios
         .post(`api/blade-food/teacherdept/submit`, {
           deptName: this.storage.name, //部门名称
-          level: this.storage.sorting //部门排序
+          level: this.storage.sorting, //部门排序
         })
-        .then(res => {
+        .then((res) => {
           this.getStorage();
           this.department = false;
           this.storage.name = "";
@@ -695,7 +708,7 @@ export default {
           console.log(res);
           this.$message({
             message: "添加成功",
-            type: "success"
+            type: "success",
           });
         })
         .catch(() => {
@@ -709,12 +722,12 @@ export default {
       this.$confirm("确认删除该部门?", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
-        type: "warning"
+        type: "warning",
       })
         .then(() => {
           this.$axios
             .get(`api/blade-food/teacherdept/remove` + addid, {})
-            .then(res => {
+            .then((res) => {
               // console.log(res);
               this.getStorage();
               this.$message.success("删除成功");
@@ -723,7 +736,7 @@ export default {
         .catch(() => {
           this.$message({
             type: "info",
-            message: "已取消删除"
+            message: "已取消删除",
           });
         });
     },
@@ -733,14 +746,14 @@ export default {
       this.view = data.id;
       this.$axios
         .get(`api/blade-food/teacherdept/detail?id=${this.view}`, {})
-        .then(res => {
+        .then((res) => {
           console.log(res);
         });
     },
     //获取部门树形结构
     getStorage() {
       this.loadFlag = true;
-      this.$axios.get(`api/blade-food/teacherdept/tree`).then(res => {
+      this.$axios.get(`api/blade-food/teacherdept/tree`).then((res) => {
         this.loadFlag = false;
         this.factors = res.data.data;
         // console.log(this.factors);
@@ -750,7 +763,7 @@ export default {
           auto[index] = {
             id: item.id,
             label: item.title,
-            into: item.level
+            into: item.level,
           };
           auto[index].children = [];
           if (item.children) {
@@ -758,7 +771,7 @@ export default {
               auto[index].children[index1] = {
                 id: item1.id,
                 label: item1.title,
-                into: item1.level
+                into: item1.level,
               };
             });
           }
@@ -766,8 +779,8 @@ export default {
         console.log(auto);
         this.data = auto;
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
