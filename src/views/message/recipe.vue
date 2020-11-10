859f6936594f559a10264a917aa2bfb4cdff6df7
huangyingfei <1297568386@qq.comm>
2020-11-10 19:50:32 +0800

    hyf

--- src/views/message/recipe.vue
+++ src/views/message/recipe.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    <h2>分享食谱</h2>
+  <div class="unsaved">
+    <div class="update">
+      名称:
+      <el-input v-model="input" placeholder="请输入内容"></el-input>
+      <span class="exact">食材名称:</span>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.unsaved {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+}
+</style>
