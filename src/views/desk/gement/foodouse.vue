24e4abbff722bb6243b75f01063a90ddcaf7f255
liqy <liqy273>
2020-12-09 23:21:47 +0800

    hyfwww

--- src/views/desk/gement/foodouse.vue
+++ src/views/desk/gement/foodouse.vue
@@ -995,15 +995,22 @@ export default {
           column.property == "stats"
         ) {
           sums[index] = values.reduce((prev, curr) => {
-            const value = Number(curr);
+            // const value = Number(curr);
+               const value =curr.toFixed(2);
+
             if (!isNaN(value)) {
               return prev + curr;
             } else {
               return prev;
             }
           }, 0);
-          sums[index] += "";
-          this.sumss = sums[index];
+          if(index==2||index==4){
+
+         
+            sums[index] = sums[index].toFixed(2);
+          }else{
+               sums[index] += "";
+          }
           // if (this.mailto[0].children[0].id == "101") {
           //   this.mailto[0].children[0].result = this.sumss;
           // }
