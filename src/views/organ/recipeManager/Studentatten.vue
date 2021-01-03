41a8514fcf24225a913baf65cc8457ff35404a7c
liqy <liqy273>
2021-01-03 22:58:58 +0800

    hyf0103

--- src/views/organ/recipeManager/Studentatten.vue
+++ src/views/organ/recipeManager/Studentatten.vue
@@ -83,7 +83,7 @@
               style="margin-left: 20px"
               type="primary"
               size="medium"
-              @click="emptyset"
+              @click="exportExcel"
               >导出</el-button
             >
           </div></el-col
@@ -93,6 +93,7 @@
         <el-col :span="24"
           ><div>
             <el-table
+            id="out-table"
               :data="tableData"
               v-loading="loadFlag"
               border
@@ -452,6 +453,9 @@
 </template>
 
 <script>
+// 引入导出Excel表格依赖
+import FileSaver from 'file-saver';
+import XLSX from 'xlsx';
 export default {
   data() {
     return {
@@ -623,6 +627,40 @@ export default {
     this.restaurants = this.loadAll();
   },
   methods: {
+       //定义导出Excel表格事件
+        exportExcel() {
+             this.$axios
+        .get(`api/blade-food/studentleave/export-template ` )
+        .then(res => {
+          console.log(res);
+          let bold= new Blob([res.data], {type: 'application/vnd.openxmlformats- officedocument.spreadsheetml.sheet;charset=utf-8'})
+          console.log(bold);
+          
+          window.location.href = window.URL.createObjectURL(bold);
+        });
+            // /* 从表生成工作簿对象 */
+            // var wb = XLSX.utils.table_to_book(document.querySelector('#out-table'),{raw:true});
+            // /* 获取二进制字符串作为输出 */
+            // var wbout = XLSX.write(wb, {
+            //     bookType: 'xlsx',
+            //     bookSST: true,
+            //     type: 'array'
+            // });
+            // try {
+            //     FileSaver.saveAs(
+            //         //Blob 对象表示一个不可变、原始数据的类文件对象。
+            //         //Blob 表示的不一定是JavaScript原生格式的数据。
+            //         //File 接口基于Blob，继承了 blob 的功能并将其扩展使其支持用户系统上的文件。
+            //         //返回一个新创建的 Blob 对象，其内容由参数中给定的数组串联组成。
+            //         new Blob([wbout], { type: 'application/octet-stream' }),
+            //         //设置导出文件名称
+            //         '学生出勤管理.xlsx'
+            //     );
+            // } catch (e) {
+            //     if (typeof console !== 'undefined') console.log(e, wbout);
+            // }
+            // return wbout;
+        },
     handleChange(value) {
       console.log(value);
     },
