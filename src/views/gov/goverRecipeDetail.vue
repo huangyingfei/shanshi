0d322ffa361268997b14b03c583b8b08f2a87ee1
Wang xuhao <15039676034@163.com>
2021-01-19 11:11:38 +0800

    w

--- src/views/gov/goverRecipeDetail.vue
+++ src/views/gov/goverRecipeDetail.vue
@@ -8,18 +8,21 @@
           style="width: 100%"
           class="recipeTableWeekData"
         >
-          <el-table-column type="index" :index="indexMethod"> </el-table-column>
-          <el-table-column prop="week1" label="周一" width="180">
+          <el-table-column prop="mealsType" label="" width="100px">
           </el-table-column>
-          <el-table-column prop="week2" label="周二" width="180">
-          </el-table-column>
+          <el-table-column prop="week1" label="周一"> </el-table-column>
+          <el-table-column prop="week2" label="周二"> </el-table-column>
           <el-table-column prop="week3" label="周三"> </el-table-column>
-          <el-table-column prop="week4" label="周四" width="180">
+          <el-table-column prop="week4" label="周四"> </el-table-column>
+          <el-table-column prop="week5" label="周五"> </el-table-column>
+          <el-table-column
+            prop="week6"
+            label="周六"
+            v-if="recipeDay == 6 || recipeDay == 7"
+          >
           </el-table-column>
-          <el-table-column prop="week5" label="周五" width="180">
+          <el-table-column prop="week7" label="周日" v-if="recipeDay == 7">
           </el-table-column>
-          <el-table-column prop="week6" label="周六"> </el-table-column>
-          <el-table-column prop="week7" label="周日"> </el-table-column>
         </el-table>
       </el-col>
     </el-row>
@@ -28,31 +31,101 @@
 
 <script>
 export default {
-  name: toolbar,
   props: {
-    transmitData: {
+    recipeData: {
       type: Object,
-      default: null,
     },
   },
   data() {
     return {
       recipeTableData: [], //弹窗
-      dialogListData: {
-        recipeName: "",
-        recipeDay: "",
-        isUse: "",
-        orgName: "",
-        createName: "",
-        createTime: "",
-      },
+      recipeDay: 5,
     };
   },
-  beforeMount() {},
+  watch: {
+    recipeData: {
+      handler: "seeRecipeInfo",
+      deep: true,
+      immediate: true,
+    },
+  },
+
   methods: {
-    indexMethod(index) {
-      let _tableLineHead = ["早餐", "早点", "午餐", "午点", "晚餐", "晚点"];
-      return _tableLineHead[index];
+    seeRecipeInfo() {
+      if (this.recipeData == null) {
+        this.recipeTableData = [];
+        return;
+      }
+      this.recipeDay = this.recipeData.recipeDay;
+      let recipeCycles = this.recipeData.recipeCycles;
+      let recipeTableData1 = [];
+      let recipeTableData = {
+        mealsType1: [],
+        mealsType2: [],
+        mealsType3: [],
+        mealsType4: [],
+        mealsType5: [],
+        mealsType6: [],
+      };
+      for (let i in recipeCycles) {
+        let mealsTypeNum = recipeCycles[i].mealsType;
+        switch (mealsTypeNum) {
+          case 1:
+            recipeTableData.mealsType1.push(recipeCycles[i]);
+            break;
+          case 2:
+            recipeTableData.mealsType2.push(recipeCycles[i]);
+            break;
+          case 3:
+            recipeTableData.mealsType3.push(recipeCycles[i]);
+            break;
+          case 4:
+            recipeTableData.mealsType4.push(recipeCycles[i]);
+            break;
+          case 5:
+            recipeTableData.mealsType5.push(recipeCycles[i]);
+            break;
+          case 6:
+            recipeTableData.mealsType6.push(recipeCycles[i]);
+            break;
+          default:
+            break;
+        }
+      }
+      var mealsTypeObj = {
+        mealsType1: "早餐",
+        mealsType2: "早点",
+        mealsType3: "午餐",
+        mealsType4: "午点",
+        mealsType5: "晚餐",
+        mealsType6: "晚点",
+      };
+      for (let key in recipeTableData) {
+        var weekData = {
+          mealsType: mealsTypeObj[key],
+          week1: "",
+          week2: "",
+          week3: "",
+          week4: "",
+          week5: "",
+          week6: "",
+          week7: "",
+        };
+
+        if (recipeTableData[key].length > 0) {
+          for (let index = 0; index < recipeTableData[key].length; index++) {
+            var weekNum = "week" + recipeTableData[key][index].week;
+
+            // weekData[weekNum].push(recipeTableData[key][index].recipeConncts[0].dishName)
+            weekData[weekNum] +=
+              recipeTableData[key][index].recipeConncts[0].dishName + "\n";
+          }
+          console.log(weekData);
+          recipeTableData1.push(weekData);
+        }
+      }
+      this.recipeTableData = recipeTableData1;
+      console.log(recipeTableData1);
     },
   },
 };
